f565d7e9ce2ad0806feece3515fad0bab9cac6c1
Andrei Lazar <andrei.lazar.1397@gmail.com>
2026-01-13 11:34:08 +0000

    managed to scrape the job urls. need to add more filters

--- scraper.py
+++ scraper.py
@@ -1,7 +1,82 @@
+""" Request Body
+{
+  "platform" : "https://www.bestjobs.eu/",
+  "keywords": ["java", "spring", "rest"],
+  "location": "timisoara",
+  "roles": ["java developer", "backend engineer", "team lead"],
+  "remote": true,
+  "hybrid": true,
+  "onsite": true,
+  "exclude": ["intern", "junior","was expired","is expired","no longer exists","This job was available"],
+  "max_results": 30
+}"""
+
+
+""" Response Body
+{
+    "results": [
+		{
+			"company": "bosch",
+			"job_url": "https://company.com/jobs/123",
+			"meta_inf": "{{key1}}:{{value1}},{{key2}}:{{value2}},{{key3}}:{{value3}},..."
+		},
+		{
+			"company": "bosch",
+			"job_url": "https://company.com/jobs/123",
+			"meta_inf": "{{key1}}:{{value1}},{{key2}}:{{value2}},{{key3}}:{{value3}},..."
+		}
+	]
+}
+	
+"""
+
 from playwright.sync_api import sync_playwright
+
+def formatRoles(filters):
+    filters["roles"] = [role.replace(" ", "-") for role in filters["roles"]]
+    return filters
+def setLocation(filters):
+    return
+
+def buildUrls (filters: map) :
+    url = "https://www.bestjobs.eu/"
+    results = []
+    filters = formatRoles(filters)
+    if(filters["location"]):
+        url+= 'locuri-de-munca-in-'+filters["location"]
+    for role in filters["roles"]:
+        url+="/"+role
+        results.append(url)
+    return results
+def getJobUrls(links):
+    results = []
+    for link in links:
+        href = link.get_attribute("href")
+        results.append(
+           {
+               "job_url":"https://www.bestjobs.eu/loc-de-munca"+href
+           }
+       )
+    return results
+
 with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
-    page.goto(" https://www.bestjobs.eu/")
-    print(page.title())
+    filters = {
+        "location": "timisoara",
+        "roles": ["java developer", "backend engineer", "team lead"],
+    }
+    filters = formatRoles(filters)
+    urls = buildUrls(filters)
+    jobUrls = []
+    for url in urls:
+        page.goto(url)
+        links = page.locator("a[href].absolute.inset-0.z-1").all()
+        jobUrls.append(getJobUrls(links))
+    response = {
+        "results": jobUrls
+    }
+    print(response)
     browser.close()
+
+    
