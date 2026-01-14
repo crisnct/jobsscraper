ec0b9a10326bede97ba7de23b2e4e77772ec11cc
Andrei Lazar <andrei.lazar.1397@gmail.com>
2026-01-14 00:45:13 +0000

    defined return funtcion type

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from typing import List
 from fastapi import FastAPI
 from types.filter import Filter
+from types.result import Result
+from types.statistic import Statistic
 
 app = FastAPI()
 
@@ -8,5 +11,7 @@ async def read_root():
     return {"Hello": "World"}
 
 @app.post("/scrape/jobs/")
-def getScrapedJobs(request_body:Filter):
+def getScrapedJobs(request_body:Filter) -> dict: 
+    results:List[Result] = []
+    statistics:List[Statistic] = []
     return {"filters":request_body}
