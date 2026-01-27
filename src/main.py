c809e4eea4736c3d0a04f402718ec53bf23ba942
Andrei Lazar <andrei.lazar.1397@gmail.com>
2026-01-27 12:02:34 +0000

    implemented request limiter with slow api. Check the comment on post request

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from scrapers.best_jobs_scrapper import BestJobsScrapper
 from models.filter import Filter
 from models.response import AppResponse
 from src.scraper_handler import ScraperHandler
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
 
 app = FastAPI()
-
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, lambda request, exc: JSONResponse(
+    status_code=429,
+    content={"detail": "Rate limit exceeded"}
+))
+app.add_middleware(SlowAPIMiddleware)
 
 
 @app.get("/")
@@ -13,6 +24,7 @@ async def read_root():
     return {"Hello": "World"}
 
 @app.post("/scrape/jobs")
+@limiter.limit("10/minute") # Limit to 10 requests per minute per IP
 async def getScrapedJobs(request_body:Filter) -> AppResponse: 
     try:
         return await BestJobsScrapper(filter=request_body).scrape()
